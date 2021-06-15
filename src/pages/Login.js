37564fc777e2d6a5cd28cc3a25f5303eea06e9a4
walterlbf <walter.lbf.f@gmail.com>
2021-06-15 00:41:32 -0400

    correcao do lint state do componente criado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       email: '',
+       password: '',
+    };
+  }
+  
+  }
   render() {
     return (
       <main>
