94b1f051aae4940c87c830e6e809c2dd19a13e83
walterlbf <walter.lbf.f@gmail.com>
2021-06-16 11:37:27 -0400

    correcao de lint, nao entendi o pq

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
-import Login from './pages/Login';
+import loginPage from './pages/Login';
 import Wallet from './pages/Wallet';
 
 export default class App extends Component {
   render() {
     return (
       <Switch>
-        <Route to="/" component={ Login } />
+        <Route to="/" component={ loginPage } />
         <Route to="/carteira" component={ Wallet } />
       </Switch>
     );
